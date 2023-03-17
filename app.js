e0c164f90eadbdfb765a2a5db9895de08b9ff165
Victor Hugo Faria Vanelli <vhvanelli@gmail.com>
2023-03-17 17:18:45 -0300

    printando corpo da requisição

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.post("/*", (req, res) => {
 
   const { body } = req;
 //   console.log(req);
-  console.log({body});
+  console.log(JSON.stringify({body}));
   axios.post(url, body, {headers: {authorization: header?.authorization}})
   .then(response =>{
       res.status(200).json(response.data);
-    console.log(response.data)
+    console.log(JSON.stringify(response.data))
   })
   .catch(err =>{
     res.status(400).json(err);
-    console.log(err)
+    console.log(JSON.stringify(err))
   })
 });
 
